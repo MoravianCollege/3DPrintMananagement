26882a0eede0ab47450e34614830dc8b6623e15b
anthonyestephan <estephana02@moravian.edu>
2020-02-22 16:42:20 -0500

    Create a method to logout from the 3dprint page

--- app.py
+++ app.py
@@ -60,14 +60,18 @@ def load_user(user_id):
 @app.route("/")
 def index():
     if current_user.is_authenticated:
-        return (
-            "<p>Hello, {}! You're logged in! Email: {}</p>"
-            "<div><p>Google Profile Picture:</p>"
-            '<img src="{}" alt="Google profile pic"></img></div>'
-            '<a class="button" href="/logout">Logout</a>'.format(
-                current_user.name, current_user.email, current_user.profile_pic
-            )
+        return render_template('main.html', name = current_user.name)
+        '''
+        (
+        "<p>Hello, {}! You're logged in! Email: {}</p>"
+        "<div><p>Google Profile Picture:</p>"
+        '<img src="{}" alt="Google profile pic"></img></div>'
+        '<a class="button" href="/logout">Logout</a>'.format(
+            current_user.name, current_user.email, current_user.profile_pic
         )
+        
+        )
+        '''
     else:
         return render_template('welcome.html')
         #return '<a class="button" href="/login">Google Login</a>'
