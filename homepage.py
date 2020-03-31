230a25b8f3f9e2a103b61ec1435dd87dcbf64a85
kschmall <schmallk@moravian.edu>
2020-03-31 13:42:05 -0400

    Runs datebase and uses flask run

--- homepage.py
+++ homepage.py
@@ -4,7 +4,7 @@ import os
 import sqlite3
 
 # Third party libraries
-from flask import Flask, redirect, request, url_for, render_template
+from flask import Flask, redirect, request, url_for, render_template, session, Blueprint
 from flask_login import (
     LoginManager,
     current_user,
@@ -16,13 +16,13 @@ from oauthlib.oauth2 import WebApplicationClient
 import requests
 
 from flask_sqlalchemy import SQLAlchemy
-from flask import session, Blueprint
 
 # Internal imports
 from .DBTableCreation import Users
 from . import db, login_manager
+from .ultimaker import Ultimaker, PrintJob, PrintJobState, PrintJobResult
 
-bp = Blueprint("homepage", __name__, url_prefix="")
+bp = Blueprint("homepage2", __name__, url_prefix="")
 
 # Configuration
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
@@ -31,12 +31,10 @@ GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
 )
 
-# User session management setup
-# https://flask-login.readthedocs.io/en/latest
-
 @login_manager.unauthorized_handler
 def unauthorized():
     return "You must be logged in to access this content.", 403
+
 
 # OAuth2 client setup
 client = WebApplicationClient(GOOGLE_CLIENT_ID)
@@ -48,17 +46,11 @@ def load_user(user_id):
 
 
 @bp.route("/")
-def index3():
+def index():
     if current_user.is_authenticated:
-        return (
-            "<p>Hello, {}! You're logged in! Email: {}</p>"
-            '<a class="button" href="/logout">Logout</a>'.format(
-                current_user.name, current_user.email
-            )
-        )
+        return render_template('form.html')
     else:
         return render_template('welcome.html')
-        #return '<a class="button" href="/login">Google Login</a>'
 
 
 @bp.route("/login")
@@ -115,19 +107,16 @@ def callback():
     # The user authenticated with Google, authorized our
     # app, and now we've verified their email through Google!
     if userinfo_response.json().get("email_verified"):
-        #unique_id = int(userinfo_response.json()["sub"])
         users_email = userinfo_response.json()["email"]
-        picture = userinfo_response.json()["picture"]
         users_name = userinfo_response.json()["name"]
     else:
         return "User email not available or not verified by Google.", 400
 
     # Create a user in our db with the information provided
     # by Google
-    
+    user = Users.query.filter_by(email=users_email).first()
 
     # Doesn't exist? Add to database
-    user = Users.query.filter_by(email=users_email).first()
     if not user:
         #User.create(unique_id, users_name, users_email, picture)
         user = Users(name=users_name, email=users_email)
@@ -148,10 +137,162 @@ def logout():
     return redirect(url_for("index"))
 
 
+@bp.route("/form")
+@login_required
+def req_form():
+    return redirect(url_for("index"))
+
+
+@bp.route("/status")
+@login_required
+def printer_status():
+    # Xerox request state
+    try:
+        Xerox = PrintJob(Ultimaker("172.31.228.191", None, None))
+        Xerox_status = Xerox.state
+        Xerox_name = Xerox.name
+        Xerox_finish = get_remaining_time(Xerox)
+       
+    except Exception as e:
+        
+        # If Printer is not doing a job
+        if str(e) == 'Not found':
+            Xerox_status = PrintJobState.NO_JOB 
+        
+        # Printer is off, UNKNOWN = OFF
+        else:
+            Xerox_status = PrintJobState.UNKNOWN
+
+        Xerox_name = ""
+        Xerox_finish = ""
+            
+    # Gutenberg request state
+    try:
+        Gutenberg = PrintJob(Ultimaker("172.31.228.190", None, None))
+        Gutenberg_status = Gutenberg.state
+        Gutenberg_name = Gutenberg.name
+        Gutenberg_finish = get_remaining_time(Gutenberg)
+        
+       
+    except Exception as e:
+        
+        # If Printer is not doing a job
+        if str(e) == 'Not found':
+            Gutenberg_status = PrintJobState.NO_JOB
+
+        # Printer is off, UNKNOWN = OFF
+        else:
+            Gutenberg_status = PrintJobState.UNKNOWN
+        
+        Gutenberg_name = ""
+        Gutenberg_finish = ""
+ 
+    Xerox_status, Gutenberg_status = get_status_string(Xerox_status, Gutenberg_status)
+    
+    return render_template('status.html',
+                           X_status=Xerox_status,
+                           G_status=Gutenberg_status,
+                           X_name=Xerox_name,
+                           G_name=Gutenberg_name,
+                           X_finish=Xerox_finish,
+                           G_finish=Gutenberg_finish)
+
+
+@bp.route("/queue")
+@login_required
+def queue():
+    return render_template('queue.html')
+
+
+@bp.route("/members")
+@login_required
+def members():
+    return render_template('members.html')
+
+
+@bp.route("/success", methods=["POST"])
+@login_required
+def success():
+    # Get results from request form
+    results = request.form
+    
+    # Check if the user actually included a print
+    if not request_has_printjob(results):   
+        return redirect(url_for("failure"))
+
+    # Print was given
+    return render_template('success.html')
+
+
+@bp.route("/error-no-print-attached")
+@login_required
+def failure():
+    return render_template('failure.html')
+
+
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 
-if __name__ == "__main__":
-    app.run(ssl_context="adhoc")
+def request_has_printjob(results):
+    # Check if has link
+    if results.get("link") == '':
+        # Check if has file
+        if results.get("files") == '':
+            # No print was given
+            return False
+        # Check the file type
+        if check_file_type_not_allowed(results.get("files")):
+            # File is neither an stl or zip file
+            return False
 
+    return True
+
+
+def check_file_type_not_allowed(file_name):
+    # If the file is not a stl file
+    if ".stl" not in file_name:
+        # If the file is not a zip file
+        if ".zip" not in file_name:
+            return True
+    
+    # The file is either an stl or a zip
+    return False
+
+
+def get_status_string(Xerox, Gutenberg):
+    Xerox_status = get_status_message(Xerox)
+    Gutenberg_status = get_status_message(Gutenberg)
+    return (Xerox_status, Gutenberg_status)
+
+
+def get_status_message(status):
+    if status == PrintJobState.NO_JOB:
+        return "The Printer is not currently working on a print"
+    elif status == PrintJobState.PRINTING:
+        return "The Printer is currently working on a print"
+    elif status == PrintJobState.PAUSING:
+        return "The Printer is pausing the print"
+    elif status == PrintJobState.PAUSED:
+        return "The Printer is currently paused"
+    elif status == PrintJobState.RESUMING:
+        return "The Printer is resuming"
+    elif status == PrintJobState.PRE_PRINT:
+        return "The Printer is currently getting ready to start a print"
+    elif status == PrintJobState.POST_PRINT:
+        return "The Printer is finished with a print"
+    elif status == PrintJobState.WAIT_CLEANUP:
+        return "The Printer is waiting for a member to clean up a finished print"
+    elif status == PrintJobState.WAIT_USER_ACTION:
+        return "The Printer is waiting for a member to reset it"
+    else:
+        return "The Printer is currently turned off"
+
+
+def get_remaining_time(print_job):
+    return ("Remaining time for print: " +
+            str(print_job.time_total - print_job.time_elapsed))
+
+if __name__ == "__main__":
+    # Run HTTPS
+    app.run(ssl_context="adhoc")
